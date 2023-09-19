73e3762250053b9addcbedd4ac41a2679ff2e77a
Gediminas <gednau@gmail.com>
2023-09-19 11:16:30 +0300

    Numbers

---
+++ kintamieji/index.js
@@ -0,0 +1,8 @@
+/*
+KINTAMIEJI
+-number
+-string (tekstas)
+-boolean (true / false)
+-array (masyvas, kolekcija, sarasas, listas, matrica)
+-object (objektas - kompleksinei informacijai aprasyti)
+*/
---
+++ kintamieji/number.js
@@ -0,0 +1,23 @@
+/*
+NUMBER
+-teigiami, neigiami
+-sveikieji, desimtainiai
+-normalus nenormalus
+
+Iniciavimo budas:
+-const(default)
+-let (jei kinta informacija)
+-var (niekada)
+*/
+
+const amzius=99;
+let pinigine=0;
+var minusas=7.56;
+
+const x1=1
+const x2=2
+
+
+console.log(amzius);
+console.log(pinige)
+console.log(minusas)
