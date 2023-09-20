96428baca4d0041e0d40221d0ee2599888896e39
Gediminas <gednau@gmail.com>
2023-09-20 10:31:01 +0300

    Aritmetika ir nodemon install

--- kintamieji/number.js
+++ kintamieji/number.js
@@ -14,10 +14,44 @@ const amzius=99;
 let pinigine=0;
 var minusas=7.56;
 
-const x1=1
-const x2=2
+const x1=10
+const x2=20
+const x3=30
+const x4=40
+const x5=50
 
+let result=x1+x4
 
-console.log(amzius);
-console.log(pinige)
+console.log(amzius)
+console.log(pinigine)
 console.log(minusas)
+console.log(result)
+
+const n1 = 7
+const n2 = 5
+// ??? = 7 + 5
+
+const sum = n1 + n2
+console.log(sum)
+
+const diff = n1 - n2
+console.log(diff)
+
+const multi = n1 * n2
+console.log(multi)
+
+const div = n1 / n2
+console.log(div)
+
+const conplex = (2 + 2) * 2
+console.log(conplex)
+
+const exp1 = 2 + 2
+const exp2 = 2 * 2
+const exp3 = 2 ** 2 
+console.log( exp1, exp2, exp3 )
+
+// 2**2 kelimas laipsniu
+
+
+
