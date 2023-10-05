91d8045d4b6f7f8351ad2a119a0d6d505bb4ca0b
Reigenas <145319460+Reigenas@users.noreply.github.com>
2023-10-05 09:54:09 +0300

    Ternary

--- funkcijos/arrow.js
+++ funkcijos/arrow.js
@@ -37,3 +37,20 @@ const kvadratu = a => a ** 2;
 
 console.log(`${n1} ** 2 = ${kvadratu(n1)}`)
 console.log(`${n2} ** 2 = ${kvadratu(n2)}`)
+
+// funkcija gauna zodi ir grazina tik pirma raide
+
+const pirmaRaide = (word) => word[0]
+console.log(pirmaRaide('Abrakadabra'))
+console.log(pirmaRaide(5))
+console.log(pirmaRaide(''))
+
+function firstLetter (str) {
+    if (typeof str !== 'string') {
+        return 'Error: first param has to be a string type'
+    }
+    if (str.length === 0) {
+        return 'Error: first param has to be non-epmty string'
+    }
+    return str[0]
+}
